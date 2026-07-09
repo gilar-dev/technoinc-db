4362a2aaf960d549d64759c6b1e7b1a5c78cc781
Gilar <gilarhafiezh2008@gmail.com>
2026-07-09 10:01:08 +0700

    feat: successfully implementing and migrating third party API from frontend to backend server and save essential credentials in .env file for better security

--- routers/cloudinary.py
+++ routers/cloudinary.py
@@ -6,9 +6,9 @@ router = APIRouter(prefix="/api/v1/cloudinary", tags=["Cloudinary"])
 
 load_dotenv()
 cloudinary.config(
-    cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
-    api_key=os.getenv("CLOUDINARY_API_KEY"),
-    api_secret=os.getenv("CLOUDINARY_API_SECRET"),
+    cloud_name=os.environ["CLOUDINARY_CLOUD_NAME"],
+    api_key=os.environ["CLOUDINARY_API_KEY"],
+    api_secret=os.environ["CLOUDINARY_API_SECRET"],
     secure=True
 )
 
